e915f3b5036a5637fa2565c993ffd6f5f20d37ae
Joshua-He <joshua5089@gmail.com>
2017-05-29 22:11:14 -0700

    Add files via upload

--- graph.cpp
+++ graph.cpp
@@ -4,8 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iomanip>
+#include "Node.h"
 
 using namespace std;
+
+static float noEdge =-99;
 
 void addVertex(char* vertex[20], int &vertexCount, char vertexInput[20]);
 void printGraph(char* vertex[20], float adTable[20][20]);
@@ -13,6 +16,8 @@ void addEdge(char* vertex[20], float adTable[20][20], float weight, char firstVe
 void removeEdge(float adTable[20][20], char firstVertex[20], char secondVertex[20], char* vertex[20]);
 void removeVertex(char* vertex[20], char removedVertex[20], float adTable[20][20]);
 int lookupVertex(char* vertex[20], char vertexInput[20]);
+void findPath(char* vertex[20], float adTable[20][20], char firstVertex[20], char secondVertex[20]);
+void printPath(Node* destination, char* vertex[20], float &totalWeight, float adTable[20][20]);
 
 int main(){
 
@@ -25,21 +30,23 @@ int main(){
   char vertex1[20];
   char vertex2[20];
   float weight = 0;
-  
+    
+  //Set array values to null
   for(int i = 0; i < 20; i++){
     vertex[i] = NULL;
   }
-  
+
+  //Initialize edge values to -99 (for no edge)
   for(int i = 0; i < 20; i++){
     for(int j = 0; j < 20; j++){
-      table[i][j] = 0;
+      table[i][j] = noEdge;
     }
   }
  
 
   //Menu
   while(inLoop == 1){
-    cout << "Enter: addvertex, addedge, removevertex, removeedge, findpath, print, exit" << endl;
+    cout << "Enter command: addvertex, addedge, removevertex, removeedge, findpath, print, exit" << endl;
     cin >> input;
     cin.ignore();
     
@@ -58,18 +65,6 @@ int main(){
       cout << "Enter weight of edge" << endl;
       cin >> weight;
       addEdge(vertex, table, weight, vertex1, vertex2);
-      /*
-      int position1 = lookupVertex(vertex, vertex1);
-      int position2 = lookupVertex(vertex, vertex2);
-      if (position1>-1 && position2 >-1) {
-	table[position1][position2] = weight;
-	cout << "Edge added with weight " << table[position1][position2] << endl; 
-      }
-      else {
-        cout << "Not valid vertex." << endl;
-      }
-      */
-	  
     }
 
     if(strcmp(input, (char*)"removevertex") == 0){
@@ -88,12 +83,17 @@ int main(){
     }
 
     if(strcmp(input, (char*)"findpath") == 0){
-
+      cout << "Enter the label of the first vertex" << endl;
+      cin >> vertex1;
+      cout << "Enter the label of the second vertex" << endl;
+      cin >> vertex2;
+      findPath(vertex, table, vertex1, vertex2);
     }
 
     if(strcmp(input, (char*)"print") == 0){
       printGraph(vertex, table);
     }
+
     if(strcmp(input, (char*)"exit") == 0){
       inLoop = 0;
       cout << "Exited" << endl;
@@ -155,14 +155,23 @@ void printGraph(char* vertex[20], float adTable[20][20]){
   for(int i = 0; i < 20; i++){
     if(vertex[i] != NULL){
       printf("%7s ", vertex[i]);
+
+      // print the columns with weight: 
       for(int j = 0; j < 20; j++){
 	if(vertex[j] != NULL){
-	  printf("%5f ", adTable[i][j]);
+	  if (adTable[i][j] != noEdge) {
+	      printf("%7.2f ", adTable[i][j]);
+	  }
+	  else {  // -99 for no edge
+	    printf("     .  ");
+	  }
 	}
       }
       cout << endl;
     }
   }
+
+  cout << endl;
 }
 
 // lookup vertex label for the array position
@@ -205,7 +214,7 @@ void addEdge(char* vertex[20], float adTable[20][20], float weight, char firstVe
    //Make sure vertex names are entered correctly
    if(position1 > -1 && position2 > -1){
      adTable[position1][position2] = weight;
-     cout << adTable[position1][position2] << endl;
+     // cout << adTable[position1][position2] << endl;
    }
    
    else{
@@ -236,8 +245,7 @@ void removeEdge(float adTable[20][20], char firstVertex[20], char secondVertex[2
 
    //Clear the edge between the two vertices
    if(position1 > -1 && position2 > -1){
-     cout << " dassda" << endl;
-     adTable[position1][position2] = 0;
+     adTable[position1][position2] = noEdge;
    }
 
    else{
@@ -265,11 +273,11 @@ void removeVertex(char* vertex[20], char removedVertex[20], float adTable[20][20
     delete vertex[position];
     vertex[position] = NULL;
     for(int i = 0; i < 20; i++){
-      adTable[i][position] = 0;
+      adTable[i][position] = noEdge;
     }
     
     for(int i = 0; i < 20; i++){
-      adTable[position][i] = 0;
+      adTable[position][i] = noEdge;
     }
     cout << "edges to the vertex have been cleared" << endl;
   }
@@ -280,4 +288,85 @@ void removeVertex(char* vertex[20], char removedVertex[20], float adTable[20][20
     return;
   }
 
+}
+
+//Breadth-first search
+void findPath(char* vertex[20], float adTable[20][20], char firstVertex[20], char secondVertex[20]){
+
+  //Initialize visited array to false
+  bool visited[20];
+  for(int i = 0; i < 20; i++){
+    visited[i] = false;
+  }
+  
+  //Find position of both vertices
+  int position1 = lookupVertex(vertex, firstVertex);
+  int position2 = lookupVertex(vertex, secondVertex);
+
+  if(position1 < 0 || position2 < 0){
+    cout << "Not valid vertices" << endl;
+    return;
+  }
+
+  Node* head = new Node;
+  head->setValue(position1);
+  Node* tail = head;
+  Node* current = head;
+  Node* destination = NULL;
+  int inLoop = 1;
+  visited[position1] = true;
+  
+  while(inLoop == 1){
+    for(int i = 0; i < 20; i++){
+      if(adTable[current->getValue()][i] != noEdge && visited[i] == false){
+	Node* n = new Node;
+	n->setParent(current);
+	n->setValue(i);
+	tail->setNext(n);
+	tail = n;
+
+	//Vertex2 found
+	if(position2 == i){
+	  destination = n;
+	  inLoop = 0;
+	}
+
+	
+      }
+      
+    }
+    current = current->getNext();
+    if(current == NULL){
+      inLoop = 0;
+    }
+    
+  }
+  
+  //No path found
+  if(destination == NULL){
+    cout << "No path found" << endl;
+  }
+
+  //Path found
+  else{
+    float totalWeight = 0;
+    cout << "Path found: ";
+    printPath(destination, vertex, totalWeight, adTable);
+    cout << endl << "Total Weight: " << totalWeight << endl;
+  }
+}
+
+void printPath(Node* destination, char* vertex[20], float &totalWeight, float adTable[20][20]){
+  
+  if(destination == NULL){
+    return;
+  }
+  else{
+    printPath(destination->getParent(), vertex, totalWeight, adTable);
+    if(destination->getParent() != NULL){
+      totalWeight = totalWeight + adTable[destination->getParent()->getValue()][destination->getValue()];
+      cout << "->";
+    }
+    cout << vertex[destination->getValue()];
+  }
 }
